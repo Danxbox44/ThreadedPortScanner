2c324ba8c442be55cad44a09a25af8fba44cb6ec
Danxbox44 <skullriper44@gmail.com>
2021-02-18 00:00:55 -0500

    Create portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+from queue import Queue
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Enter Target IP Address')
+parser.add_argument('target', help='IP Address of Target')
+args = parser.parse_args()
+input_target = args.target
+target = input_target
+
+queue = Queue()
+open_ports = []
+
+opening_message = f"""
+---------------------------------------------------
+Threaded port scanner made by Cyber44, Version 2.0! 
+Target: {target}
+---------------------------------------------------
+"""
+print(opening_message)
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+
+def fill_queue(port_list):
+    for port in port_list:
+        queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print(f" Port {port} on {target} is open")
+            open_ports.append(port)
+
+port_list = range(1, 20000)
+fill_queue(port_list)
+
+thread_list = []
+
+
+for t in range(900):
+    thread = threading.Thread(target=worker)
+    thread_list.append(thread)
+
+for thread in thread_list:
+    thread.start()
+
+for thread in thread_list:
+    thread.join()
+
+nmap_ports = str(open_ports).replace("[", "").replace("]", "").replace(" ",'')
+
+os.system(f"nmap -sC -T5 -sV -p {nmap_ports} {target}")
